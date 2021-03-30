adb2ddf6e63930a1345327a49e8cb2bbf654123a
Jose Barbosa <kidchenko@gmail.com>
2021-03-30 14:55:40 +0700

    Create choco.ps1

---
+++ choco.ps1
@@ -0,0 +1,15 @@
+choco feature enable -n=allowGlobalConfirmation
+
+choco install postman
+
+choco install vscode
+
+choco install vim
+
+choco install hyper
+
+choco install nvm
+
+choco install python
+
+choco feature disable -n=allowGlobalConfirmation
