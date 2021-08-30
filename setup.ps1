7475c007eb227c4e0b65f44e8c8aa271cfabd817
Jose Barbosa <kidchenko@gmail.com>
2021-08-30 15:57:00 +0700

    Create setup.ps1

---
+++ setup.ps1
@@ -0,0 +1,14 @@
+Install-PackageProvider -Name NuGet -Force
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
+
+Install-Module -Name PowerShellGet -Force
+
+Update-Module -Name PowerShellGet
+
+Install-Module -Name z
+
+Install-Module -Name posh-git
+
+Install-Module -Name oh-my-posh
+
