b59984aca2d5cfe2f02f91c449b7b5332b54fe7c
kidchenko <kidchenko@gmail.com>
2021-09-05 06:13:15 +0700

    ask to install and add comments

--- tools/install.sh
+++ tools/install.sh
@@ -5,7 +5,14 @@ say() {
 	echo $1
 }
 
+ask() {
+	echo
+	read -p "$*" -n 1 -r
+	echo
+}
+
 warn() {
+	say
 	printf >&2 "$SCRIPTNAME: $*\n"
 }
 
@@ -14,6 +21,8 @@ iscmd() {
 }
 
 checkdeps() {
+	say
+	say "checking dependencies..."
 	# https://ss64.com/bash/local.html
 	local -i not_found
 	for cmd; do
@@ -21,24 +30,36 @@ checkdeps() {
 		iscmd "$cmd" || {
 			warn $"$cmd is not found"
 			let not_found++
-			say $not_found
 		}
 	done
+	# same as if ()
 	((not_found == 0)) || {
-		warn $"Install dependencies listed above to use $SCRIPTNAME"
+		warn $"The dependencies listed above are required to use $SCRIPTNAME"
+		ask $"Do you wanna to install?"
+		if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+			say "install the dependencies and then continue..."
+			[[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1 # handle exits from shell or function but don't exit interactive shell
+		fi
+
 	}
+
 }
 
 installdeps() {
-	say "installdeps 1"
-	say "installing..."
+	say
+	say "installing deps..."
+	# same as for var in "$@"
+	# https://stackoverflow.com/questions/255898/how-to-iterate-over-arguments-in-a-bash-script
+	for dep; do
+		say "installing dep $dep"
+	done
 }
 
 main() {
 	say "hello world"
-	say "checking dependencies..."
+
 	checkdeps git brew juca
-	installdeps juca
+	installdeps juca brew
 }
 
 main
