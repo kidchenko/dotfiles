5debbdd5f17a5c1e223867c044a7dc73f9ee962a
kidchenko <kidchenko@gmail.com>
2021-09-05 04:23:29 +0700

    Check deps in install.ps1

--- tools/install.ps1
+++ tools/install.ps1
@@ -1,6 +1,32 @@
+function Warn([string]$message) {
+	Write-Warning $message
+}
+
+function IsCommand([string]$cmd) {
+	return which $cmd
+}
+
+function CheckDeps([string[]]$deps) {
+	foreach ($cmd in $deps) {
+		Write-Host "checking if $cmd is installed"
+		if (!(IsCommand $cmd)) {
+			Warn "$cmd is not found"
+		}
+	}
+}
+
+function InstallDeps ([string[]]$deps) {
+	"installing..."
+	foreach ($cmd in $deps) {
+		Write-Host "installing $cmd"
+	}
+}
+
 function Main {
 	"hello world"
-	"installing..."
+	Write-Output "checking dependencies..."
+	CheckDeps git, choco, juca
+	InstallDeps choco, juca
 }
 
 Main
