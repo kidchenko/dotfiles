b52bdc03956ae00e348550af05ca882b662201a3
kidchenko <kidchenko@gmail.com>
2021-09-28 03:17:13 +0700

    Add update

---
+++ tools/update.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+# Default settings
+REPO=${REPO:-kidchenko/dotfiles}
+DOTFILES_DIR=${DOTFILES_DIR:-~/.${REPO}}
+REMOTE=${REMOTE:-https://github.com/${REPO}.git}
+BRANCH=${BRANCH:-master}
+
+main() {
+    echo
+    pushd $DOTFILES_DIR > /dev/null
+
+    # check for updates
+    fetch=$(git fetch --dry-run 2>&1)
+
+    if !(test -z "$fetch"); then
+        echo "[dotfiles] New version available."
+        read -p $"[dotfiles] Would you like to update? [y/n]: " -n 1 -r
+        echo
+        if [[ $REPLY =~ ^[Yy]$ ]]; then
+            echo "[dotfiles] Updating..."
+            echo
+            git pull -r
+            source $DOTFILES_DIR/setup.sh
+        fi
+    fi
+
+    popd > /dev/null
+}
+
+main
