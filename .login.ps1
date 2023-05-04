8e9eb7647cdb1b7a4e1f7b123e67c08033213acc
kidchenko <kidchenko@gmail.com>
2023-05-04 18:27:32 +0700

    Add aws autocomplete

--- .login.ps1
+++ .login.ps1
@@ -24,3 +24,19 @@ if (!($IsMacOS)) {
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072;
 
 oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/$Theme" | Invoke-Expression
+
+# enable AWS command completion
+# https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-completion.html
+Register-ArgumentCompleter -Native -CommandName aws -ScriptBlock {
+    param($commandName, $wordToComplete, $cursorPosition)
+        $env:COMP_LINE=$wordToComplete
+        if ($env:COMP_LINE.Length -lt $cursorPosition){
+            $env:COMP_LINE=$env:COMP_LINE + " "
+        }
+        $env:COMP_POINT=$cursorPosition
+        aws_completer.exe | ForEach-Object {
+            [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+        }
+        Remove-Item Env:\COMP_LINE
+        Remove-Item Env:\COMP_POINT
+}
